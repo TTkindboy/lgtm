5cd0f931d747eb2e105d36845a45cd9155907dce
TTkindboi <taklipstein@icloud.com>
2026-03-02 19:48:03 -0500

    Seperate css

--- src/lgtm/title.py
+++ src/lgtm/title.py
@@ -5,36 +5,9 @@ from pyfiglet import figlet_format
 
 
 class TitleScreen(App):
-    CSS = """
-    Screen {
-        align: center middle;
-    }
-
-    #root {
-        width: 100%;
-        height: 100%;
-        align: center middle;
-    }
-
-    #title {
-        text-style: bold;
-        content-align: center middle;
-    }
-
-    #tagline {
-        margin-top: 3;
-        opacity: 0.75;
-        content-align: center middle;
-    }
-
-
-    #hint {
-        margin-top: 2;
-        opacity: 0.6;
-        content-align: center middle;
-    }
-    """
-
+    CSS_PATH = "title.tcss"
+    BINDINGS = [("enter", "start", "Start game")]
+    
     def compose(self) -> ComposeResult:
         yield Container(
             Static(figlet_format("LGTM", font="ansi_regular").rstrip(), id="title"),
@@ -43,6 +16,5 @@ class TitleScreen(App):
             id="root",
         )
 
-    def on_key(self, event) -> None:
-        if event.key == "enter":
-            self.exit()
+    def action_start(self) -> None:
+        self.exit()
