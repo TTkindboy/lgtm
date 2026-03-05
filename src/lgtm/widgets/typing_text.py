b4c91cf9cada2fb4f8e486ecdf6f09d6948fbd30
TTkindboi <taklipstein@icloud.com>
2026-03-05 18:51:54 -0500

    Use render() instead of update

--- src/lgtm/widgets/typing_text.py
+++ src/lgtm/widgets/typing_text.py
@@ -1,25 +1,21 @@
-from textual.visual import VisualType
-from time import monotonic
 from textual.reactive import reactive
 from textual.widgets import Static
 
 
-class TypingText(Static): # TODO: FIX EXTERIOR UPDATE BEHAVIOR WHEN 
-    start_time: reactive[float] = reactive(monotonic)  # ty:ignore[invalid-assignment]
-    time = reactive(0.0)
-
-    def on_mount(self):
-        self.set_interval(1 / 60, self.update_time)
-    
-    def update_time(self):
-        self.time = monotonic() - self.start_time
-    
-    def watch_time(self, time: float):
-        num_chars = int(self.time // self.seconds_per_char)
-        # idk if this checks if it changed? maybe some fancy reactive magic with render() should be used here instead.
-        self.update(self.text[:min(num_chars, len(self.text))])
+class TypingText(Static): # TODO: FIX EXTERIOR UPDATE BEHAVIOR 
+    index = reactive(0)
     
     def __init__(self, content: str, speed: int = 30, **kwargs):
-        self.text = content
+        self._text = content
         self.seconds_per_char = 1 / speed
         super().__init__(content, **kwargs)
+
+    def _advance(self):
+        self.index+=1
+    
+    def on_mount(self):
+        self.set_interval(self.seconds_per_char, self._advance, repeat=len(self._text))
+    
+
+    def render(self) -> str:
+        return self._text[:self.index]
